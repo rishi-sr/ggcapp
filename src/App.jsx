d88630d718c699d5b47309dd3a33a284a38eb7bb
rishi-sr <rishi16esrivastava@gmail.com>
2026-04-26 01:25:43 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,11 @@
-import React, { useState, useEffect } from "react";
-import "./App.scss";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import AdminPanel from "./pages/AdminPanel";
-import Login from "./pages/Login";
-import JudgePanel from "./pages/JudgePanel";
-import Leaderboard from "./pages/LeaderBoard";
-import ProtectedRoute from "./components/ProtectedRoute";
-
-import { auth } from "./firebase";
-import { onAuthStateChanged } from "firebase/auth";
-import ResultReveal from "./pages/RevealPage";
+import React from 'react'
 
 const App = () => {
-  const [videoDone, setVideoDone] = useState(false);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      setLoading(false);
-
-      if (user) {
-        localStorage.setItem("userEmail", user.email);
-      }
-    });
-
-    return () => unsubscribe();
-  }, []);
-
-  if (loading) return <h2>Loading...</h2>;
-
   return (
-    <>
-      {!videoDone && (
-        <div className="video-preloader">
-          <video
-            src="/ggcpreloader.mp4"
-            autoPlay
-            muted
-            playsInline
-            onEnded={() => setVideoDone(true)}
-          />
-        </div>
-      )}
+    <div>
+      
+    </div>
+  )
+}
 
-      {videoDone && (
-        <Router>
-          <Routes>
-            <Route path="/" element={<Login />} />
-
-            
-            
-
-            
-
-            
-          </Routes>
-        </Router>
-      )}
-    </>
-  );
-};
-
-export default App;
+export default App
